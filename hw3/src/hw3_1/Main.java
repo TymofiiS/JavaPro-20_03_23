9c09b07d86a85bf6696a54746d7ca3933c1753fc
Tymofii Serhiienko <tymofii.serhiienko@gmail.com>
2023-04-05 23:00:08 +0300

    continue dev

---
+++ hw3/src/hw3_1/Main.java
@@ -0,0 +1,10 @@
+package hw3_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		var employee = 
+				new Employee(null, null, null, null, null);
+	}
+}
